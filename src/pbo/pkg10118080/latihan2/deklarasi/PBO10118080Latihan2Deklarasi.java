542f7818fed06eacbe1067edf3df3e9d689d1b97
Taufiq R <taufiq.it2@gmail.com>
2019-09-24 19:23:25 +0700

    first commit

---
+++ src/pbo/pkg10118080/latihan2/deklarasi/PBO10118080Latihan2Deklarasi.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118080.latihan2.deklarasi;
+
+/**
+ *
+ * @author 
+ * Nama :Taufiq Rizky
+ * NIM  :10118080
+ * Deskripsi Program    : program menampilkan isi variable
+ */
+public class PBO10118080Latihan2Deklarasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //deklarasi
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        //inisialisasi
+        nilaiInt=78;
+        nilaiLogika=false;
+        nilaiKarakter='D';
+        //output
+        System.out.println();
+        System.out.println("Isi Variable Nilai      :"+nilaiInt);
+        System.out.println("Isi Variable PHI        :"+PHI);
+        System.out.println("Isi Variable Logika     :"+nilaiLogika);
+        System.out.println("Isi Variable Karakter   :"+nilaiKarakter);
+  
+    }
+    
+}
